45d123bcf62ab69f7e8a8e2042db6b153cc6e8a1
Daniel Ran <danielr1735@gmail.com>
2018-08-11 18:21:01 -0400

    add class Multigame, passed to Game

--- classes.js
+++ classes.js
@@ -1,60 +1,90 @@
+// ******** MultiGame ********
 
-// ******** Game ******** TODO: should be level sensitive.
+class MultiGame {
+  constructor(playerName = 'Akiva Frederick Hastings Foss Feig Entin Rein') {
+    this.playerName = playerName
+  }
+
+  run() {
+    let level = 1
+    let game = new Game(this, level)
+    game.run()
+  }
+}
+
+// ******** Game ********
 
 class Game {
-    constructor(level) {
+    constructor(multiGame, level) {
+      this.multiGame = multiGame
       this.level = level
+      this.attackCount = 0
+      this.bombs = []
     }
 
     run() {
       let info = document.getElementById('game-name')
-      info.innerHTML = `Toe Tac Tic Tic Boom ! ! !       Level: ${this.level}`
-        if (attackCount < 2) {
-            let attack = new Attack
-            attackCount++
-            attack.run()
+      info.innerHTML = `Toe Tac Tic Tic Boom ! ! !<br>Player:${this.multiGame.playerName}<br> Level: ${this.level}`
+        if (this.attackCount < 2) {
+            this.attack()
+            this.attackCount++
         } else {
-          info.innerHTML = "Done!"
+          info.innerHTML = `Level ${this.level} Done!`
           setTimeout(() => {
-            attackCount = 0
-            bombs = []
-            run()
+            generate(1)
           }, 2000)
         }
     }
-}
 
+    attack(maxInterval = 3000, bombCount = randFromOneTo(9)) {
+      let ids = selectRandomNums(bombCount)
 
-// ******** Attack ******** //
-class Attack {
-    constructor(maxInterval = 3000, bombCount = randFromOneTo(9)) {
-      this.maxInterval = maxInterval
-      this.bombCount = bombCount
-      this.ids = selectRandomNums(bombCount)
-    }
-
-    run() {
-      this.ids.forEach((id) => {
+      ids.forEach((id) => {
         setTimeout(() => {
                           let bomb = new Bomb(id, 3, 1000);
                           bomb.run()
                           },
-                  randFromOneTo(this.maxInterval)
+                  randFromOneTo(maxInterval)
         )
       })
     }
 
-  }
+}
+
+
+
+// ******** Attack ******** //
+// class Attack {
+//     constructor(maxInterval = 3000, bombCount = randFromOneTo(9)) {
+//       this.maxInterval = maxInterval
+//       this.bombCount = bombCount
+//       this.ids = selectRandomNums(bombCount)
+//     }
+//
+//     run() {
+//       this.ids.forEach((id) => {
+//         setTimeout(() => {
+//                           let bomb = new Bomb(id, 3, 1000);
+//                           bomb.run()
+//                           },
+//                   randFromOneTo(this.maxInterval)
+//         )
+//       })
+//     }
+//
+//   }
 
 
 // ******** Bomb ******** //
+
+
 class Bomb {
 
   constructor(id, startAt = 10, interval = 1000) {
     this.id = id
     this.startAt = startAt
     this.interval = interval
-    bombs.push(this)
+    // bombs.push(this)
   }
 
   run() {
@@ -70,8 +100,8 @@ class Bomb {
                               for (let i = 0; i < nodes.length; i++) {
                                 nodes[i].innerHTML = ''
                               }
-                              bombs = []
-                              run()
+                              // bombs = []
+                              main()
                             }, 1000
                     )
           }
--- main.js
+++ main.js
@@ -1,37 +1,7 @@
-// function runSingleGame(level) {
-//       //
-//       // let cells = document.getElementById("grid").childNodes
-//       // cells.forEach(cell => cell.className = "countdown")
-//       let header = document.getElementById("game-name")
-//       header.innerHTML = `ToeTacTicTicBoom!! Level: ${level}`
-//       interval = 1000 - (level * 100)
-//       generateCountdowns(interval)
-// }
-
-
-// function runMultiLevelGame(startingLevel) {
-//   let level = startingLevel
-//   // for (let level = startingLevel; level <= 9; level++) {
-//     runSingleGame(level)
-//     // while (true) {
-//     //   if (gameRunning == false) {
-//     //     runSingleGame(++level)
-//     //   }
-//   }
-
-
-// let gameRunning = false
-// runMultiLevelGame(1)
-
-function run() {
-
-  let game = new Game(1)
-  game.run()
-
+function main() {
+  let multiGame = new MultiGame
+  multiGame.run()
 }
 
-// single game variables:
-let attackCount = 0
-let bombs = []
 let nodes = document.getElementById("grid").getElementsByTagName('div')
-run()
+main()
